459ebe486b6a52e37b4d15ee94f944138e9c760c
marlonjt <marlonj3@outlook.es>
2023-04-11 13:48:34 -0500

    uncomment .py

--- formula.py
+++ formula.py
@@ -8,10 +8,10 @@ myList = [1, 8, 9.8, 8, 10]
 res = []
 print(len(myList))
 
-# for n in range(len(myList)):
-#     for r in range(len(myList)):
-#         if n != r:
-#             if myList[n] == myList[r] and myList[n] not in res:
-#                 res.append(myList[n])
-#                 print(res)
+for n in range(len(myList)):
+    for r in range(len(myList)):
+        if n != r:
+            if myList[n] == myList[r] and myList[n] not in res:
+                res.append(myList[n])
+                print(res)
 
