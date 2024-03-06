5caf617b9960df7b8d3e1472b63b3b9c6fb5b69c
marlonjt <marlonj3@outlook.es>
2024-03-06 15:55:46 -0500

    funciones string y test generate pass

--- funciones_string.py
+++ funciones_string.py
@@ -25,3 +25,51 @@ peso = int(input("Ingrese su peso: "))
 altura = float(input("Ingrese su altura: "))
 masa_corporal = peso / (altura * altura)
 print("Tu índice de masa corporal es donde " + str(round(masa_corporal, 2)))
+
+# frase ingresada e imprimido en sentido contrario
+word = input("ingrese la para imprimir en reversa: ")
+
+
+def phrase_reverse(word):
+    result = word.split()  # eliminar elementos de string y convirtiéndolo en un array
+    return result[::-1]
+
+
+# aplicación de join para que imprima en forma de string y no de array
+print(" ".join(phrase_reverse(word)))
+
+# aplicando método reverse en función
+word = input("ingrese la para imprimir en reversa: ")
+
+
+def phrase_reverse(word):
+    result = word.split()  # eliminar elementos de string y convirtiéndolo en un array
+    result.reverse()
+    return " ".join(result)
+
+
+# aplicación de join para que imprima en forma de string y no de array
+print(phrase_reverse(word))
+
+
+# funciones con listas y iteraciones  con condicionales
+lista = [1, 2, 5, 3, 3, 8, 9, 10, 2]
+
+
+# función para buscar elementos duplicados y agregar en un nuevo array
+def duplicado(x):
+    d = []
+    for i in x:
+        if i not in d:
+            d.append(i)
+    return d
+
+
+# set para interceptar números duplicados en un función
+def duplicado_v2(x):
+    return list(set(x))
+
+
+print(lista)
+print(duplicado(lista))
+print(duplicado_v2(lista))
